8494e165746ffe78bbbe0c2ba602ee56f365b221
sahil-vandra <sahilvandra.softvan@gmail.com>
2022-04-06 15:24:20 +0530

    03:25 commit

--- scripts/install_os_dependencies.sh
+++ scripts/install_os_dependencies.sh
@@ -5,5 +5,5 @@ source newenv/bin/activate
 yum install -y python3-pip sqlitebrowser
 # sudo pip3 install -r requirements.txt
 sudo pip3 install django
-sudo chmod +x /home/ec2-user/manage.py
+# sudo chmod +x /home/ec2-user/manage.py
 # sudo python3 /home/ec2-user/manage.py migrate
--- scripts/start_application.sh
+++ scripts/start_application.sh
@@ -4,6 +4,5 @@
 # source /home/ec2-user/www/project-venv/bin/activate
 # echo yes | DJANGO_SETTINGS_MODULE=project.settings.staging SECRET_KEY=your-secret-here JWT_SECRET_KEY=your-jwt-secret-here PSQL_DB_NAME=your-db-name-here PSQL_DB_USER=your-db-user-here PSQL_DB_PASSWD=your-db-password-here PSQL_HOST=your-aws-psql-rds-server-dns-here PSQL_PORT=5432 /home/ec2-user/www/project/manage.py collectstatic
 # DJANGO_SETTINGS_MODULE=project.settings.staging SECRET_KEY=your-secret-here JWT_SECRET_KEY=your-jwt-secret-here PSQL_DB_NAME=your-db-name-here PSQL_DB_USER=your-db-user-here PSQL_DB_PASSWD=your-db-password-here PSQL_HOST=your-aws-psql-rds-server-dns-here PSQL_PORT=5432 supervisord -c /home/ec2-user/www/project/supervisor/default.conf
-
-sudo chmod +x manage.py
+sudo chmod +x /home/ec2-user/manage.py
 sudo python3 manage.py runserver
