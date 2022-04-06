f38e7a8c063efd001fb286344f57486c4aa631ed
sahil-vandra <sahilvandra.softvan@gmail.com>
2022-04-06 19:12:28 +0530

    07:12 commit

--- scripts/install_os_dependencies.sh
+++ scripts/install_os_dependencies.sh
@@ -2,7 +2,7 @@ sudo yum install python3 pip3
 sudo pip3 install virtualenv 
 virtualenv newenv
 source newenv/bin/activate
-yum install -y python3-pip sqlitebrowser
+# yum install -y python3-pip
 # sudo pip3 install -r requirements.txt
 sudo pip3 install django
 # sudo chmod +x /home/ec2-user/manage.py
--- scripts/start_application.sh
+++ scripts/start_application.sh
@@ -2,4 +2,4 @@
 # sudo chmod +x /home/ec2-user/manage.py
 cd /home/ec2-user/
 # ls
-sudo python3 manage.py runserver
+sudo python3 manage.py runserver &
