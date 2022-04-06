05c0e11bde2cb227e3c1ab0914f92abacf7bc33f
sahil-vandra <sahilvandra.softvan@gmail.com>
2022-04-06 16:08:51 +0530

    04:08 commit

--- helloworld/settings.py
+++ helloworld/settings.py
@@ -71,16 +71,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'helloworld.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'helloworld.db'),
-    }
-}
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
