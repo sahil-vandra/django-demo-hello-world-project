622034f5cab96373d7fa0fbb22b80e5d53777672
sahil-vandra <sahilvandra.softvan@gmail.com>
2022-04-06 12:56:04 +0530

    12:55 commit

--- scripts/install_os_dependencies.sh
+++ scripts/install_os_dependencies.sh
@@ -1,4 +1,7 @@
+virtualenv newenv
+source newenv/bin/activate
 yum install -y python3-dev python3-pip python3-virtualenv sqlitebrowser
 pip install -r requirements.txt
+pip install django
 python3 manage.py migrate
 python3 manage.py runserver
