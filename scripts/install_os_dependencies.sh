67a7cb60bf93467713029206bcd530536a53bf94
sahil-vandra <sahilvandra.softvan@gmail.com>
2022-04-06 16:09:41 +0530

    04:09 commit

--- scripts/install_os_dependencies.sh
+++ scripts/install_os_dependencies.sh
@@ -5,5 +5,3 @@ source newenv/bin/activate
 yum install -y python3-pip sqlitebrowser
 # sudo pip3 install -r requirements.txt
 sudo pip3 install django
-sudo chmod +x /home/ec2-user/manage.py
-sudo python3 /home/ec2-user/manage.py migrate
