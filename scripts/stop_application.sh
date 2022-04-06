e77b2732f0a6878436ef0264ca44dfc59ff765bc
sahil-vandra <sahilvandra.softvan@gmail.com>
2022-04-06 13:01:16 +0530

    01:01 commit

--- scripts/stop_application.sh
+++ scripts/stop_application.sh
@@ -3,4 +3,4 @@
 # source /home/ec2-user/www/project-venv/bin/activate
 # # supervisorctl -c /home/ec2-user/www/project/supervisor/default.conf stop all 2&>1 >/dev/null
 # # sudo unlink /tmp/supervisor.sock 2> /dev/null
-sudo pkill supervisor*
+# sudo pkill supervisor*
